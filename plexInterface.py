7e10b647d06ab9b0be482a41ae551bf1a05b3d4b
Miriad <zcorneli@iit.edu>
2012-11-21 16:52:23 -0600

    Changed from generic mkv transcoder to m3u8 (http-live-streaming) transcoder. Transcoding working, ish

--- plexInterface.py
+++ plexInterface.py
@@ -1,7 +1,7 @@
 import web, urllib2, re, xml.etree.cElementTree as et
 from urllib import urlencode
 from urlparse import urlparse
-import uuid
+import uuid, hmac, hashlib, base64, time 
 
 class PlexMedia:
     def __init__(self, mediaurl):
@@ -27,37 +27,56 @@ class PlexMedia:
         self.width = self.mediaTag.attrib['width']
         self.height = self.mediaTag.attrib['height']
         self.title = self.videoTag.attrib['title']
-        self.continueTime= self.videoTag.attrib['viewOffset']
+        try:
+            self.continueTime= self.videoTag.attrib['viewOffset']
+        except KeyError:
+            pass
         self.duration = int(self.partTag.attrib['duration'])
         self.playedURL = parsed_path.scheme + "://" + parsed_path.netloc + "/:/scrobble?key=" + str(self.mediaKey) + "&identifier=com.plexapp.plugins.library"
         self.updateURL =  parsed_path.scheme + "://" + parsed_path.netloc + '/:/progress?key=' + str(self.mediaKey) + '&identifier=com.plexapp.plugins.library&time=%s&state=playing' 
-        self.transcodeURL = parsed_path.scheme + "://" + parsed_path.netloc + '/video/:/transcode/generic.%s?'
+        self.transcodeBaseURL = parsed_path.scheme + "://" + parsed_path.netloc
+        self.transcodeURL = '/video/:/transcode/segmented/start.m3u8?'
 
 
-    def getTranscodeURL(self, format='mkv', videoCodec='libx264', audioCodec=None, continuePlay=False, continueTime=None, videoWidth='1280', videoHeight='720', videoBitrate=None):
+    def getTranscodeURL(self, extension='mkv', format='matroska', videoCodec='libx264', audioCodec=None, continuePlay=False, continueTime=None, videoWidth='1280', videoHeight='720', videoBitrate=None):
         if(videoWidth > self.width):
             videoWidth = self.width
         if(videoHeight > self.height):
             videoHeight = self.height
         self.session = uuid.uuid4()
         args = dict()
-        args["width"] = videoWidth
-        args["height"] = videoHeight
-        args["format"] = format
-        args["fakeContentLength"] = self.duration
-        args['key'] = self.mediaKey
-        args["session"] = "%s" % self.session.hex
-        args["videoBitrate"] = 12000
-        args['videoCodec'] = videoCodec
+#        args["width"] = videoWidth
+#        args["height"] = videoHeight
+#        args["format"] = format
+#        args["fakeContentLength"] = self.duration * 1000
+#        args['key'] = self.mediaKey
+#        args["session"] = "%s" % self.session.hex
+#        args["videoBitrate"] = 12000
+#        args['videoCodec'] = videoCodec
+        args['offset'] = 0
+        args['3g'] = 0
+        args['subtitleSize'] = 125
+        args['secondsPerSegment'] = 10
+        args['ratingKey'] = self.mediaKey
+        args['key'] = self.transcodeBaseURL + "/library/metadata/%s" % self.mediaKey
         args["identifier"] = "com.plexapp.plugins.library"
-        args["quality"] = 11
+        args["quality"] = 7
         args["url"] = self.fileURL
-        if(audioCodec):
-            args["audioCodec"] = audioCodec
-        else:
-            args["audioCodec"] = self.audio_codec
-        transcodeURL = self.transcodeURL % (format)
+#        if(audioCodec):
+#            args["audioCodec"] = audioCodec
+#        else:
+#            args["audioCodec"] = self.audio_codec
+        transcodeURL = self.transcodeURL
         transcodeURL += urlencode(args)
+        atime = int(time.time())
+        message = transcodeURL + "@%d" % atime
+        sig = base64.b64encode(hmac.new(PlexInterface.transcode_private, msg=message, digestmod=hashlib.sha256).digest())
+        plexAccess = dict()
+        plexAccess['X-Plex-Access-Key'] = PlexInterface.transcode_public
+        plexAccess['X-Plex-Access-Time'] = atime
+        plexAccess['X-Plex-Access-Code'] = sig
+        plexAccess['X-Plex-Client-Capabilities'] = 'protocols=http-live-streaming,http-mp4-streaming,http-mp4-video,http-mp4-video-720p,http-streaming-video,http-streaming-video-720p;videoDecoders=h264{profile:high&resolution:1080&level:41};audioDecoders=aac,mp3,ac3,dts'
+        transcodeURL = self.transcodeBaseURL + transcodeURL + "&" + urlencode(plexAccess)
         return transcodeURL
 
     def setPlayed(self):
@@ -78,6 +97,8 @@ class PlexMedia:
         
 
 class PlexInterface:
+    transcode_private = base64.b64decode('k3U6GLkZOoNIoSgjDshPErvqMIFdE0xMTx8kgsrhnC0=')
+    transcode_public = 'KQMIY6GATPC63AIMC4R2'
 
     def __init__(self):
         pass
