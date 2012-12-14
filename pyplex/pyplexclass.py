1bda3570950aeb2dd6714ad8df34bfbefb2a0f69
Bram Wubs <bwubs@me.com>
2012-12-14 14:57:42 +0100

    Started on a pyPlex wrapper class

---
+++ pyplex/pyplexclass.py
@@ -0,0 +1,12 @@
+from commands.xbmc import xbmcCommands
+from gui.image import image
+# from interfaces.plexInterface
+# from listners.httplistner import
+class pyPlex():
+	"""Wrapper class for pyPlex"""
+	def __init__(self, arg):
+		pass
+	
+	def start(self):
+		"""Start pyPlex and all other stuff"""
+		pass
