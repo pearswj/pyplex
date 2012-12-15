7e06acaf8aaf423f2555dca388f767cfea851575
megawubs <bwubs@me.com>
2012-12-15 15:26:24 +0100

    Deleted code leftovers

--- pyplex.py
+++ pyplex.py
@@ -1,21 +1,8 @@
-# import urllib2, re, xml.etree.cElementTree as et
-# from pyplex.pyomx.pyomxplayer import OMXPlayer
-# # from pyomxplayer import OMXPlayer
-# from urlparse import urlparse
-# # import avahi, dbus, sys, platform
-# from pprint import pprint
-# import socket, subprocess, signal, os, logging
-# from threading import Thread
-# import Queue
-# import udplistener
-# import httplistener
-# from pyplex.commands.xbmc import xbmcCommands
-# from pyplex.interfaces.plexInterface import PlexInterface
 from pyplex.interface import pyPlex
 import sys
 from pprint import pprint
 
-#this is kind of my idea...
+
 if __name__ == "__main__":
 	args = sys.argv
 	pprint(args)
@@ -25,20 +12,3 @@ if __name__ == "__main__":
 		plex.run()
 	except:
 		"error while trying to start pyplex"
-
-# exit()
-# urls = (
-#     '/xbmcCmds/xbmcHttp','xbmcCmdsXbmcHttp',
-#     '/(.*)', 'stop', 'hello'
-# )
-
-# class hello:        
-#     def GET(self, message):
-#         return 'Hello, World'
-
-# http = None
-# udp = None
-
-# if __name__ == "__main__":
-    
-
